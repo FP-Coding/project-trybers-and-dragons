7474fe80c6af6afe589424770bbde503aee7770d
Felipe Pinto <felipe14062000@gmail.com>
2023-02-16 15:39:03 -0300

    feat: criação da Class Monster

--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -4,7 +4,6 @@ import SimpleFighter from './SimpleFighter';
 export default interface Fighter extends SimpleFighter {
   defense: number,
   energy?: Energy,
-  attack(enemy: Fighter): void,
   special?(enemy: Fighter): void,
   levelUp(): void,
 }
