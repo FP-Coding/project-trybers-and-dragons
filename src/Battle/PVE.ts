c9b37e07f282b01e681263c8be2e230b6f8dc3c3
Felipe Pinto <felipe14062000@gmail.com>
2023-02-16 18:33:04 -0300

    feat: criando Class PVE

---
+++ src/Battle/PVE.ts
@@ -0,0 +1,41 @@
+import Fighter, { SimpleFighter } from '../Fighter';
+import Battle from './Battle';
+
+class PVE extends Battle {
+  constructor(
+    player: Fighter,
+    private _enviroment: (Fighter | SimpleFighter)[],
+  ) {
+    super(player);
+  }
+
+  static verifyIsAlive(enemy: (Fighter | SimpleFighter)) {
+    return enemy.lifePoints !== -1;
+  }
+  
+  private attackMonsters(enviroment: (Fighter | SimpleFighter)[]) {
+    enviroment.forEach((enemy) => {
+      if (enemy.lifePoints !== -1) { this.player.attack(enemy); }
+    });
+  }
+
+  private attackPlayer(enviroment: (Fighter | SimpleFighter)[]) {
+    enviroment.forEach((enemy) => {
+      enemy.attack(this.player);
+    });
+  }
+
+  fight(): number {    
+    let battle = true;
+    const verificationEnemies = this._enviroment.every(PVE.verifyIsAlive);
+    const verificationPlayer = PVE.verifyIsAlive(this.player); 
+    while (battle) {
+      this.attackMonsters(this._enviroment);
+      this.attackPlayer(this._enviroment);
+      if (verificationEnemies || verificationPlayer) battle = false;
+    }
+    return super.fight(); 
+  }
+}
+
+export default PVE;
